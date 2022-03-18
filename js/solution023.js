9b85df81540a40cbf1282fbf12ca19d4211232f4
cwellis <cwellis2016@gmail.com>
2022-03-17 21:44:36 -0400

    solution 023

--- js/solution023.js
+++ js/solution023.js
@@ -1,8 +1,8 @@
-// function grow(x){
-//     let product = 1
+function grow(x){
+    let product = 1
     
-//     for (i = 0; i < x.length; i++) {
-//       product *= x[i]
-//     }
-//     return product
-// }
+    for (i = 0; i < x.length; i++) {
+      product *= x[i]
+    }
+    return product
+}
