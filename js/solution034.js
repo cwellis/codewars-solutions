5cb8088a108c5fce9e1ddb4fb007c8f02909b178
cwellis <cwellis2016@gmail.com>
2022-03-29 21:55:44 -0400

    solution 034

---
+++ js/solution034.js
@@ -0,0 +1,11 @@
+function points(games) {
+    let total = 0;
+    games.map(game => {
+      if (game[0] === game[2]) {
+        total += 1;
+      } else if (game[0] > game[2]) {
+        total += 3;
+      }
+    });
+    return total;
+  }
