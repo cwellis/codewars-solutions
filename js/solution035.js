9f68f5556303b2e02ad11329f2481acbbe6b5b4a
cwellis <cwellis2016@gmail.com>
2022-03-30 21:56:31 -0400

    solution 035

---
+++ js/solution035.js
@@ -0,0 +1,5 @@
+const well = x => {
+    const good_count = x.filter(x => x == 'good').length;
+    return good_count < 1 ? 'Fail!' : 
+           good_count < 3 ? 'Publish!' : 'I smell a series!';
+  }
