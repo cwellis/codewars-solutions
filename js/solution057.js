eb88426731f3436950370dbf265984370784c1ef
cwellis <cwellis2016@gmail.com>
2022-04-20 22:16:08 -0400

    solution 057

---
+++ js/solution057.js
@@ -0,0 +1,11 @@
+function mergeArrays(arr1, arr2) {
+    return Array.from(new Set(arr1.concat(arr2).sort((a,b) => (a-b))));
+  }
+
+function mergeArrays(arr1, arr2) {
+    return Array.from(new Set(arr1.concat(arr2).sort((a,b) => (a-b))))
+}
+
+function mergeArrays(arr1, arr1) {
+    return Array.from(new Set(arr1.concat(arr2).sort((a,b) => (a-b))))
+}
