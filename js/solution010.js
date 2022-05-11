2521474c82583cd8b6fd27b1acd9b89c37f9e22b
cwellis <cwellis2016@gmail.com>
2022-05-11 07:16:03 -0400

    removes numbers that include a 5

---
+++ js/solution010.js
@@ -0,0 +1,15 @@
+// function isNarcissistic(n){
+//     //your code here
+//     let arr = n.toString().split('')
+//     let res = 0
+    
+//     for (i = 0; i < arr.length; i++) {
+//       res += (Math.pow(arr[i], arr.length))
+//     }
+    
+//     if (res == n) {
+//       return true
+//     } else {
+//       return false
+//     }
+//   }
