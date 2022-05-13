8c9b6b42cad36afe6d9c58c2b80610a74cf4f9f5
cwellis <cwellis2016@gmail.com>
2022-05-13 08:34:47 -0400

    solution 12

--- js/solution012.js
+++ js/solution012.js
@@ -1,13 +1,13 @@
-// function bulbMaze(map){
-//     for (let i = 0; i < map.length; i+=2){
-//       if (map[i] === "o"){
-//         return false
-//       }
-//       }
-//    for (let j = 1; j < map.length; j+= 2){
-//      if (map[j] === "x"){
-//        return false
-//      }
-//    }
-//      return true
-//     }
+function bulbMaze(map){
+    for (let i = 0; i < map.length; i+=2){
+      if (map[i] === "o"){
+        return false
+      }
+      }
+   for (let j = 1; j < map.length; j+= 2){
+     if (map[j] === "x"){
+       return false
+     }
+   }
+     return true
+    }
