c2637b92e620e7bfa956c6bc1a2b5100ed866dbb
cwellis <cwellis2016@gmail.com>
2022-05-14 10:58:25 -0400

    solution 013

--- js/solution013.js
+++ js/solution013.js
@@ -1,9 +1,9 @@
-// function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin){
+function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin){
   
-//     if (dolphin == true) {
-//       sharkSpeed /= 2
-//     }
+    if (dolphin == true) {
+      sharkSpeed /= 2
+    }
     
-//     return pontoonDistance / youSpeed < sharkDistance / sharkSpeed ? 'Alive!' : 'Shark Bait!'
+    return pontoonDistance / youSpeed < sharkDistance / sharkSpeed ? 'Alive!' : 'Shark Bait!'
     
-//   }
+  }
