1f22b10940d16257473045e6d802e6e6f027816e
cwellis <cwellis2016@gmail.com>
2022-05-18 21:34:19 -0400

    solution 018

--- js/solution018.js
+++ js/solution018.js
@@ -1,8 +1,8 @@
-// function repeatStr (n, s) {
-//     let statement = ''
+function repeatStr (n, s) {
+    let statement = ''
     
-//     for (i = 0; i < n; i++) {
-//       statement += `${s}`
-//     }
-//     return statement
-//   }
+    for (i = 0; i < n; i++) {
+      statement += `${s}`
+    }
+    return statement
+  }
