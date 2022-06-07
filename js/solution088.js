ab79b24ce3e63d322ae88397f01b3090ae827dc0
cwellis <cwellis2016@gmail.com>
2022-06-06 21:16:24 -0400

    solution 088

--- js/solution088.js
+++ js/solution088.js
@@ -1,9 +1,9 @@
-// function duplicateEncode(word){
-//     return word
-//       .toLowerCase()
-//       .split('')
-//       .map( function (a, i, w) {
-//         return w.indexOf(a) == w.lastIndexOf(a) ? '(' : ')'
-//       })
-//       .join('');
-//   }
+function duplicateEncode(word){
+    return word
+      .toLowerCase()
+      .split('')
+      .map( function (a, i, w) {
+        return w.indexOf(a) == w.lastIndexOf(a) ? '(' : ')'
+      })
+      .join('');
+  }
