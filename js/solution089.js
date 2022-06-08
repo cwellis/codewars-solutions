1a25a51add572ba69f02d3317dcadefdf3989742
cwellis <cwellis2016@gmail.com>
2022-06-07 21:16:43 -0400

    solution 089

--- js/solution089.js
+++ js/solution089.js
@@ -1,10 +1,10 @@
-// function alphabetPosition(text) {
-//     var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-//     return text.toLowerCase().split('')
-//     .filter( letter => {
-//       let index = alphabet.indexOf(letter);
-//       return index > -1;
-//     }  )
-//     .map( letter => alphabet.indexOf(letter) + 1 )
-//     .join(' ')
-//   }
+function alphabetPosition(text) {
+    var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    return text.toLowerCase().split('')
+    .filter( letter => {
+      let index = alphabet.indexOf(letter);
+      return index > -1;
+    }  )
+    .map( letter => alphabet.indexOf(letter) + 1 )
+    .join(' ')
+  }
