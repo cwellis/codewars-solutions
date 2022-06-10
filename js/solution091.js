c350adc5653ff3723c949d275775ec4b524415bf
cwellis <cwellis2016@gmail.com>
2022-06-09 21:17:28 -0400

    solution 091

--- js/solution091.js
+++ js/solution091.js
@@ -1,6 +1,6 @@
-// function isValidWalk(walk) {
-//     function count(val) {
-//       return walk.filter(function(a){return a==val;}).length;
-//     }
-//     return walk.length==10 && count('n')==count('s') && count('w')==count('e');
-//   }
+function isValidWalk(walk) {
+    function count(val) {
+      return walk.filter(function(a){return a==val;}).length;
+    }
+    return walk.length==10 && count('n')==count('s') && count('w')==count('e');
+  }
