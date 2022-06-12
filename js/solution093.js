66c607b52baff64dd065da89caf886f38819ba99
cwellis <cwellis2016@gmail.com>
2022-06-11 21:18:06 -0400

    solution 093

--- js/solution093.js
+++ js/solution093.js
@@ -1,12 +1,12 @@
-// function order(words){
-//     var array = words.split(' ');
-//     var sortedArray = [];
-//     for(i = 0; i <= array.length; i++) {
-//       for(j = 0; j < array.length; j++) {
-//         if(array[j].indexOf(i) >= 0) {
-//           sortedArray.push(array[j]);
-//         }
-//       }
-//     }
-//     return sortedArray.join(' ');
-//   }
+function order(words){
+    var array = words.split(' ');
+    var sortedArray = [];
+    for(i = 0; i <= array.length; i++) {
+      for(j = 0; j < array.length; j++) {
+        if(array[j].indexOf(i) >= 0) {
+          sortedArray.push(array[j]);
+        }
+      }
+    }
+    return sortedArray.join(' ');
+  }
