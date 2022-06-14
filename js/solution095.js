2fe17ceb1e415bf2e697874bd697ff0389b760e1
cwellis <cwellis2016@gmail.com>
2022-06-13 21:18:53 -0400

    solution 095

--- js/solution095.js
+++ js/solution095.js
@@ -1,6 +1,6 @@
-// function isPangram(string){
-//     string = string.toLowerCase();
-//     return "abcdefghijklmnopqrstuvwxyz".split("").every(function(x){
-//       return string.indexOf(x) !== -1;
-//     });
-//   }
+function isPangram(string){
+    string = string.toLowerCase();
+    return "abcdefghijklmnopqrstuvwxyz".split("").every(function(x){
+      return string.indexOf(x) !== -1;
+    });
+  }
