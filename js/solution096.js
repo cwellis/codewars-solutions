9b53a9066b18e11f8963eef3218ceb4a01b54382
cwellis <cwellis2016@gmail.com>
2022-06-14 21:19:11 -0400

    solution 096

--- js/solution096.js
+++ js/solution096.js
@@ -1,15 +1,15 @@
-// function digPow(n, p){
-//     // ...
-//     var num1=n.toString();
-//     var num2=p.toString();
-//     var sum=0;
+function digPow(n, p){
+    // ...
+    var num1=n.toString();
+    var num2=p.toString();
+    var sum=0;
     
-//     for(var i=0;i<num1.length;i++){
-//       sum=sum+Math.pow(parseInt(num1.charAt(i)),p);
-//       p++;
-//       if(sum%n==0){return sum/n;}
-//     }
-//     return -1;
+    for(var i=0;i<num1.length;i++){
+      sum=sum+Math.pow(parseInt(num1.charAt(i)),p);
+      p++;
+      if(sum%n==0){return sum/n;}
+    }
+    return -1;
     
     
-//   }
+  }
