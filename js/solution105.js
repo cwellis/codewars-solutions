4ba99a0d18060419fcef223f3368259d0efdf11c
cwellis <cwellis2016@gmail.com>
2022-06-28 18:11:55 -0400

    solution

--- js/solution105.js
+++ js/solution105.js
@@ -1,4 +1,4 @@
-// function solution(start, finish) {
-//   let remainder = (finish - start) % 3;
-//   return ((finish - start) - remainder) / 3 + remainder;
-// }
+function solution(start, finish) {
+  let remainder = (finish - start) % 3;
+  return ((finish - start) - remainder) / 3 + remainder;
+}
