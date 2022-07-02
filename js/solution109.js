f1ea547328cfc153a5466ec5c0f1a93679823ece
cwellis <cwellis2016@gmail.com>
2022-07-02 18:13:36 -0400

    solution

--- js/solution109.js
+++ js/solution109.js
@@ -1,16 +1,16 @@
-// function basicOp(operation, value1, value2)
-// {
-//   // Code
-//   if( operation == "+") {
-//     return value1 + value2
-//   }
-//   if( operation == "-") {
-//     return value1 - value2
-//   }
-//   if( operation == "*") {
-//     return value1 * value2
-//   }
-//   if( operation == "/") {
-//     return value1 / value2
-//   }
-// }
+function basicOp(operation, value1, value2)
+{
+  // Code
+  if( operation == "+") {
+    return value1 + value2
+  }
+  if( operation == "-") {
+    return value1 - value2
+  }
+  if( operation == "*") {
+    return value1 * value2
+  }
+  if( operation == "/") {
+    return value1 / value2
+  }
+}
