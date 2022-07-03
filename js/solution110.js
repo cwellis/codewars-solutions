8045899d797a3610517a989664eec52fe31c9e97
cwellis <cwellis2016@gmail.com>
2022-07-03 18:14:01 -0400

    solution

--- js/solution110.js
+++ js/solution110.js
@@ -1,7 +1,7 @@
-// function findNextSquare(sq) {
-//     var number = Math.sqrt(sq);
-//     if(Math.round(number) === number) {
-//       return Math.pow(++number, 2)
-//     }
-//     return -1;
-//   }
+function findNextSquare(sq) {
+    var number = Math.sqrt(sq);
+    if(Math.round(number) === number) {
+      return Math.pow(++number, 2)
+    }
+    return -1;
+  }
