c33d11ddf1e96c8f9802c87441fe4fd6984b3d0d
cwellis <cwellis2016@gmail.com>
2022-07-06 18:14:57 -0400

    solution

--- js/solution113.js
+++ js/solution113.js
@@ -1,8 +1,8 @@
-// function oddOrEven(array) {
-//     var result = 0;
-//     for (var i = 0; i < array.length; i++)
-//     {result+=array[i];}
-//     if (result%2 == 0)
-//     {return "even";}
-//     else{return "odd";}
-//   }
+function oddOrEven(array) {
+    var result = 0;
+    for (var i = 0; i < array.length; i++)
+    {result+=array[i];}
+    if (result%2 == 0)
+    {return "even";}
+    else{return "odd";}
+  }
