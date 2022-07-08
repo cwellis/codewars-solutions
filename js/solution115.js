76578a66799c0a169a4300c39db5768ba04c67f7
cwellis <cwellis2016@gmail.com>
2022-07-08 18:15:40 -0400

    solution

--- js/solution115.js
+++ js/solution115.js
@@ -1,10 +1,10 @@
-// function find_average(array) {
-//     if (array.length === 0) {
-//     return 0;
-//     }
-//     var result = 0;
-//     for (i=0; i<array.length; i++) {
-//       result +=array[i];
-//     }
-//     return result/array.length;
-//   }
+function find_average(array) {
+    if (array.length === 0) {
+    return 0;
+    }
+    var result = 0;
+    for (i=0; i<array.length; i++) {
+      result +=array[i];
+    }
+    return result/array.length;
+  }
