a97916a01946481148a23c9abe6469416ebc8ccc
cwellis <cwellis2016@gmail.com>
2022-07-09 18:15:56 -0400

    solution

--- js/solution116.js
+++ js/solution116.js
@@ -1,14 +1,14 @@
-// function bmi(weight, height) {
-//     var result = weight/Math.pow(height,2) 
+function bmi(weight, height) {
+    var result = weight/Math.pow(height,2) 
     
-//     if (result <= 18.5) {
-//       return "Underweight";
-//     } else if (result <= 25) {
-//       return "Normal";
-//     } else if (result <= 30) {
-//       return "Overweight";
-//     } else {
-//       return "Obese";
-//     }
+    if (result <= 18.5) {
+      return "Underweight";
+    } else if (result <= 25) {
+      return "Normal";
+    } else if (result <= 30) {
+      return "Overweight";
+    } else {
+      return "Obese";
+    }
     
-//   }
+  }
