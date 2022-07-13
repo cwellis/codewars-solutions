e132bbf4e318db3bf4101c827a2226fa8e106111
cwellis <cwellis2016@gmail.com>
2022-07-13 18:16:58 -0400

    solution

--- js/solution120.js
+++ js/solution120.js
@@ -1,3 +1,3 @@
-// function greet (name, owner) {
-//     return name === owner ? 'Hello boss' : 	'Hello guest';
-//   }
+function greet (name, owner) {
+    return name === owner ? 'Hello boss' : 	'Hello guest';
+  }
