2ffb2276899985a08444d4a18ed820d02215740c
cwellis <cwellis2016@gmail.com>
2022-08-05 18:26:39 -0400

    solution

--- js/solution156.js
+++ js/solution156.js
@@ -1,14 +1,14 @@
-// function nextPal(val) {
-//     do {
-//       val++;
-//     } while(!isPalindrome(val))
-//     return val;
-//   }
+function nextPal(val) {
+    do {
+      val++;
+    } while(!isPalindrome(val))
+    return val;
+  }
   
-//   function isPalindrome(num) {
-//     return num === reverseNum(num);
-//   }
+  function isPalindrome(num) {
+    return num === reverseNum(num);
+  }
   
-//   function reverseNum(num) {
-//     return parseInt(num.toString().split('').reverse().join(''));
-//   }
+  function reverseNum(num) {
+    return parseInt(num.toString().split('').reverse().join(''));
+  }
