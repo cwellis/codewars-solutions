c1d2baa4b7e63972416c2937c2474f406da6c425
cwellis <cwellis2016@gmail.com>
2022-08-07 18:28:04 -0400

    solution

--- js/solution154.js
+++ js/solution154.js
@@ -1,9 +1,9 @@
-// function wallpaper (length, width, height) {
-//     if (length === 0 || width === 0) return 'zero'
+function wallpaper (length, width, height) {
+    if (length === 0 || width === 0) return 'zero'
   
-//     const roomArea = 2 * (length + width) * height
-//     const rollArea = 0.52 * 10
+    const roomArea = 2 * (length + width) * height
+    const rollArea = 0.52 * 10
   
-//     const rollsRequired = Math.ceil(roomArea / rollArea * 1.15)
-//     return numbers[rollsRequired]
-//   }
+    const rollsRequired = Math.ceil(roomArea / rollArea * 1.15)
+    return numbers[rollsRequired]
+  }
