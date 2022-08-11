782cd1bfe73493a52f734534c347d2073e94572d
cwellis <cwellis2016@gmail.com>
2022-08-11 18:28:48 -0400

    solution

--- js/solution141.js
+++ js/solution141.js
@@ -1,5 +1,5 @@
-// var humanYearsCatYearsDogYears = function(y) {
-//     if (y == 1) return [1, 15, 15]
-//     if (y == 2) return [2, 24, 24]
-//     return [y, (y-2) * 4 + 24, (y-2) * 5 + 24]
-//   }
+var humanYearsCatYearsDogYears = function(y) {
+    if (y == 1) return [1, 15, 15]
+    if (y == 2) return [2, 24, 24]
+    return [y, (y-2) * 4 + 24, (y-2) * 5 + 24]
+  }
