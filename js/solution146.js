dd4a9addcc70a51aac6694c2e4759313524a22e2
cwellis <cwellis2016@gmail.com>
2022-08-23 18:32:26 -0400

    solution

--- js/solution146.js
+++ js/solution146.js
@@ -1,3 +1,3 @@
-// function divisibleBy(numbers, divisor) {
-//     return numbers.filter(n => n % divisor === 0)
-//   }
+function divisibleBy(numbers, divisor) {
+    return numbers.filter(n => n % divisor === 0)
+  }
