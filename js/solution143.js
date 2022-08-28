ac5768f53b2ded5cfe43cb9ba3455ba3c943c8e9
cwellis <cwellis2016@gmail.com>
2022-08-28 18:33:50 -0400

    solution

--- js/solution143.js
+++ js/solution143.js
@@ -1,4 +1,4 @@
-// function index(array, n){
-//     //your code here
-//     return array[n] ** n || -1;
-//   }
+function index(array, n){
+    //your code here
+    return array[n] ** n || -1;
+  }
