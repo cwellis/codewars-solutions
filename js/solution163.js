199380ef3b5f68c9b9c6cc24f179521cab10cf1f
cwellis <cwellis2016@gmail.com>
2022-09-01 20:08:32 -0400

    solution

--- js/solution163.js
+++ js/solution163.js
@@ -1,3 +1,3 @@
-// const eqSumPowdig = (hMax, exp) => [...Array(hMax - 1)]
-//   .map((_, idx) => idx + 2)
-//   .filter(num => [...`${num}`].reduce((sum, val) => sum + Math.pow(val,exp), 0) === num);
+const eqSumPowdig = (hMax, exp) => [...Array(hMax - 1)]
+  .map((_, idx) => idx + 2)
+  .filter(num => [...`${num}`].reduce((sum, val) => sum + Math.pow(val,exp), 0) === num);
