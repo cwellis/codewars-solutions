87e97278810efa45cd84411e5ecec74c102d9dae
cwellis <cwellis2016@gmail.com>
2022-09-02 20:08:49 -0400

    solution

--- js/solution164.js
+++ js/solution164.js
@@ -1,9 +1,9 @@
-// function nextNumb(val) { 
-//     while (val < 9876543210) {
-//       val++;
-//       if(val%2 && !(val%3) && val.toString().match(/^(?!.*(.).*\1)\d{1,10}$/)) {
-//         return val;
-//       }
-//     }
-//     return 'There is no possible number that fulfills those requirements';
-//   }
+function nextNumb(val) { 
+    while (val < 9876543210) {
+      val++;
+      if(val%2 && !(val%3) && val.toString().match(/^(?!.*(.).*\1)\d{1,10}$/)) {
+        return val;
+      }
+    }
+    return 'There is no possible number that fulfills those requirements';
+  }
