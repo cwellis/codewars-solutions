9217b2c3a343a18607c1df786c9220bc40818e12
cwellis <cwellis2016@gmail.com>
2022-09-15 20:13:17 -0400

    solution

--- js/solution175.js
+++ js/solution175.js
@@ -1,3 +1,3 @@
-// function howManyLightsabersDoYouOwn(name) {
-//     return name === 'Zach' ? 18 : 0;
-//   }
+function howManyLightsabersDoYouOwn(name) {
+    return name === 'Zach' ? 18 : 0;
+  }
