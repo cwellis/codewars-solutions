48580f19990651420f7599ac8edbd106b090da6a
cwellis <cwellis2016@gmail.com>
2022-09-17 20:13:38 -0400

    solution

--- js/solution176.js
+++ js/solution176.js
@@ -1,4 +1,4 @@
-// function spoonerize(words) {
-//     let word = words.split(' ')
-//     return [word[1][0] + word[0].slice(1), word[0][0] + word[1].slice(1)].join(' ')
-// }
+function spoonerize(words) {
+    let word = words.split(' ')
+    return [word[1][0] + word[0].slice(1), word[0][0] + word[1].slice(1)].join(' ')
+}
