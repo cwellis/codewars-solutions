2d7fecaa975f12c7194e4482df77dabfb3970ebd
cwellis <cwellis2016@gmail.com>
2022-09-20 20:15:38 -0400

    solution

--- js/solution178.js
+++ js/solution178.js
@@ -1,7 +1,7 @@
-// const getPositions = s => {
-//     const firstPerson = Math.trunc(s % 3);
-//     const secondPerson = Math.trunc((s / 3) % 3);
-//     const thirdPerson = Math.trunc((s / 9) % 3);
+const getPositions = s => {
+    const firstPerson = Math.trunc(s % 3);
+    const secondPerson = Math.trunc((s / 3) % 3);
+    const thirdPerson = Math.trunc((s / 9) % 3);
   
-//     return [firstPerson, secondPerson, thirdPerson];
-//   }
+    return [firstPerson, secondPerson, thirdPerson];
+  }
