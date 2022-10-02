8edd7a964dbc77cd74f9f132e32cbeb9b9e410ad
cwellis <cwellis2016@gmail.com>
2022-10-01 20:18:46 -0400

    solution

--- js/solution208.js
+++ js/solution208.js
@@ -1,3 +1,3 @@
-// function apple(x){
-//     return Math.pow(x, 2) > 1000 ? 'It\'s hotter than the sun!!' : 'Help yourself to a honeycomb Yorkie for the glovebox.' ;
-//   }
+function apple(x){
+    return Math.pow(x, 2) > 1000 ? 'It\'s hotter than the sun!!' : 'Help yourself to a honeycomb Yorkie for the glovebox.' ;
+  }
