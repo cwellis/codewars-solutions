8241185b687c3af7e1d250f1a521f15a2f0c0628
cwellis <cwellis2016@gmail.com>
2022-10-04 20:19:59 -0400

    solution

--- js/solution205.js
+++ js/solution205.js
@@ -1,8 +1,8 @@
-// function preFizz(n) {
-//     var output = [];
-//     for (var i=1; i<=n; i++)
-//     {
-//       output.push(i);
-//     }
-//     return output;
-//   }
+function preFizz(n) {
+    var output = [];
+    for (var i=1; i<=n; i++)
+    {
+      output.push(i);
+    }
+    return output;
+  }
