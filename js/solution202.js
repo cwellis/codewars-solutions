6d49ceea1339803466aaa20f52c2ad92169486b7
cwellis <cwellis2016@gmail.com>
2022-10-05 20:20:30 -0400

    solution

--- js/solution202.js
+++ js/solution202.js
@@ -1,5 +1,5 @@
-// var laLigaGoals = 43
-// var championsLeagueGoals = 10
-// var copaDelReyGoals = 5
+var laLigaGoals = 43
+var championsLeagueGoals = 10
+var copaDelReyGoals = 5
 
-// var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
