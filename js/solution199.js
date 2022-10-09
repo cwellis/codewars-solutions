4880641d5752cbd11fab426196ff1ecdf0917b31
cwellis <cwellis2016@gmail.com>
2022-10-08 20:21:39 -0400

    solution

--- js/solution199.js
+++ js/solution199.js
@@ -1,13 +1,13 @@
-// function findMaxProduct(arr){
-//     const product = [];
-//     let sum = 1;
-//     for (let k = 0; k < arr.length; k++) {
-//       for (let i = k; i < arr.length; i += k + 1) {
-//         sum *= arr[i];
-//       }
-//       product.push(sum);
-//       sum = 1;
-//     }
-//     return Math.max(...product);
+function findMaxProduct(arr){
+    const product = [];
+    let sum = 1;
+    for (let k = 0; k < arr.length; k++) {
+      for (let i = k; i < arr.length; i += k + 1) {
+        sum *= arr[i];
+      }
+      product.push(sum);
+      sum = 1;
+    }
+    return Math.max(...product);
   
-//   }
+  }
