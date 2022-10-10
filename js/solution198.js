64ed320bd928ad2325a5c646fe4ed6f5a882ee3e
cwellis <cwellis2016@gmail.com>
2022-10-09 20:22:20 -0400

    solution

--- js/solution198.js
+++ js/solution198.js
@@ -1,4 +1,4 @@
-// function contact(hallway) {
-//     const gaps = hallway.match(/>-*</g)
-//     return !gaps ? -1 : Math.min(...gaps.map(s=>s.length)) >> 1
-//   }
+function contact(hallway) {
+    const gaps = hallway.match(/>-*</g)
+    return !gaps ? -1 : Math.min(...gaps.map(s=>s.length)) >> 1
+  }
