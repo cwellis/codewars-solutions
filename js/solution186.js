c315049b896c8e02a4d7176f00ae6300ccfe3c92
cwellis <cwellis2016@gmail.com>
2022-11-13 20:27:36 -0500

    solution

--- js/solution186.js
+++ js/solution186.js
@@ -1,19 +1,19 @@
-// function largest(n,xs){
-//     // Find the n highest elements in a list
-//     var len = xs.length;    
-//     // Basic bubble sort in O(n^2)
-//     for(var i = len ; i >= 0 ; --i){
-//         for(var j = 0; j < i ; j++){
-//             if(xs[j] > xs[j + 1]){
-//                 // Swaping position n && n+1
-//                 var swap = xs[j];
-//                 xs[j] = xs[j+1];
-//                 xs[j + 1] = swap;            
-//             }
-//         }
-//     }
+function largest(n,xs){
+    // Find the n highest elements in a list
+    var len = xs.length;    
+    // Basic bubble sort in O(n^2)
+    for(var i = len ; i >= 0 ; --i){
+        for(var j = 0; j < i ; j++){
+            if(xs[j] > xs[j + 1]){
+                // Swaping position n && n+1
+                var swap = xs[j];
+                xs[j] = xs[j+1];
+                xs[j + 1] = swap;            
+            }
+        }
+    }
     
     
-//     return xs.slice(len - n)
+    return xs.slice(len - n)
       
-//   }
+  }
