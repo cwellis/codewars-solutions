eef4683fdee86a186636b3fd33ed387d960cc5cd
cwellis <cwellis2016@gmail.com>
2022-11-22 20:29:01 -0500

    solution

--- js/solution182.js
+++ js/solution182.js
@@ -1,18 +1,18 @@
-// function fizzbuzz(n)
-// {
-//   var i = 1, arr = [];
-//   while(i <= n) {
-//     var fizz = (i % 3 == 0);
-//     var buzz = (i % 5 == 0);
-//     if(fizz || buzz) {
-//       arr.push((fizz?"Fizz":"") + (buzz?"Buzz":""));
-//     }
-//     else {
-//       arr.push(i);
-//     }
-//     i++;
-//   }
-//   return arr;
-// }
+function fizzbuzz(n)
+{
+  var i = 1, arr = [];
+  while(i <= n) {
+    var fizz = (i % 3 == 0);
+    var buzz = (i % 5 == 0);
+    if(fizz || buzz) {
+      arr.push((fizz?"Fizz":"") + (buzz?"Buzz":""));
+    }
+    else {
+      arr.push(i);
+    }
+    i++;
+  }
+  return arr;
+}
 
-// var fizzify = fizzbuzz;
+var fizzify = fizzbuzz;
