6c1973f2cbf8b9360c214b24c3cc16025466690a
cwellis <cwellis2016@gmail.com>
2022-11-26 20:39:02 -0500

    solution

--- js/solution216.js
+++ js/solution216.js
@@ -1,14 +1,14 @@
-// const xMarksTheSpot = (input) => {
-//     let coords = []
+const xMarksTheSpot = (input) => {
+    let coords = []
   
-//     for (let i = 0; i < input.length; i++) {
-//       if (input[i].includes("x")) {
-//         coords.push([i, input[i].indexOf("x")])
-//       }
-//     }
-//     if (coords.length === 1) {
-//       return coords[0]
-//     } else {
-//       return []
-//     }
-//   }
+    for (let i = 0; i < input.length; i++) {
+      if (input[i].includes("x")) {
+        coords.push([i, input[i].indexOf("x")])
+      }
+    }
+    if (coords.length === 1) {
+      return coords[0]
+    } else {
+      return []
+    }
+  }
