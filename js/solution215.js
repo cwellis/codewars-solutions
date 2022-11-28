30561af8b440cb6f1ea842c2f2ef3b4f9b580c54
cwellis <cwellis2016@gmail.com>
2022-11-27 20:39:17 -0500

    solution

--- js/solution215.js
+++ js/solution215.js
@@ -1,13 +1,13 @@
-// const gps = (s, x) => {
+const gps = (s, x) => {
 
-//     if (x.length<=1) {
-//       return 0;
-//     }
+    if (x.length<=1) {
+      return 0;
+    }
     
-//     let output = [];
-//     for (let i = 0; i < x.length-1; i++) { 
-//       output.push((x[i+1]-x[i])*3600/s);
-//     }
+    let output = [];
+    for (let i = 0; i < x.length-1; i++) { 
+      output.push((x[i+1]-x[i])*3600/s);
+    }
     
-//     return Math.max(...output);
-//   }
+    return Math.max(...output);
+  }
